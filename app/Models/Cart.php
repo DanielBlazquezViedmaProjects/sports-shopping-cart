8e565865d0bd443e9b1e0536d0f863467031c4b7
Daniel Blazquez <danielblazquez@bonsaigrupo.com>
2023-06-25 21:43:42 +0200

    Rest of files

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,7 +2,49 @@
 
 namespace App\Models;
 
-class Cart
-{
+use http\Message;
 
+class Cart{
+
+    private $items;
+
+    public function __construct(){
+        $this->items = [];
+    }
+
+    public function addItem($productId, $quantity){
+        if (isset($this->items[$productId])){
+            $this->items[$productId]+= $quantity;
+        }
+        $this->items[$productId] = $quantity;
+    }
+
+    public function updateItem($productId, $quantity){
+        if (isset($this->items[$productId])){
+            $this->items[$productId] = $quantity;
+        }
+    }
+
+    public function removeItem($productId){
+        if (isset($this->items[$productId])){
+           unset( $this->items[$productId]);
+        }
+    }
+
+    public function getTotalItems(){
+        return count($this->items);
+    }
+
+    public function confirmPurchase(){
+        $this->items = [];
+
+        echo "item purchased";
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems(): array{
+        return $this->items;
+    }
 }
