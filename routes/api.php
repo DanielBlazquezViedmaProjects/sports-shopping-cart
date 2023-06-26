47d68beba279b81ec4d88ffa3b2c970bf520c559
Daniel Blazquez <danielblazquez@bonsaigrupo.com>
2023-06-26 12:35:54 +0200

    testing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test-session', function () {
+    session(['user_id' => 1, 'username' => 'john.doe']);
+    return "Sesión de prueba guardada.";
+});
+
+Route::post('/cart', [CartController::class, 'addToCart']);
+Route::prefix('cart')->group(function () {
+    Route::post('/add', [CartController::class, 'addToCart']);
+    Route::put('/update/{productId}', [CartController::class, 'updateCart']);
+    Route::delete('/remove/{productId}', [CartController::class, 'removeFromCart']);
+    Route::get('/total', [CartController::class, 'getTotalProducts']);
+    Route::post('/confirm', [CartController::class, 'confirmPurchase']);
 });
